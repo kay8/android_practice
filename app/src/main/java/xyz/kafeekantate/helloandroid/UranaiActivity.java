a5543996f9dc1473994d58e8e258543746318fb8
kay8 <kay.yamagishi.work@gmail.com>
2017-08-07 18:06:24 -0700

    add uranai logic

--- app/src/main/java/xyz/kafeekantate/helloandroid/UranaiActivity.java
+++ app/src/main/java/xyz/kafeekantate/helloandroid/UranaiActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
+
 public class UranaiActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +14,22 @@ public class UranaiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uranai);
 
+        Random random = new Random();
+        int n = random.nextInt(4);
+
+        String fortune;
+        if (n == 0) {
+            fortune = "大吉";
+        } else if (n == 1) {
+            fortune = "中吉";
+        } else if (n == 2) {
+            fortune = "小吉";
+        } else {
+            fortune = "凶";
+        }
+
+
         TextView text = (TextView) findViewById(R.id.uranaiText);
-        text.setText("中吉");
+        text.setText(fortune);
     }
 }
