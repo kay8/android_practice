5f34905cd7ad85da8f28da6ccf98a3675d6a0483
kay8 <kay.yamagishi.work@gmail.com>
2017-08-07 17:50:57 -0700

    change text

--- app/src/main/java/xyz/kafeekantate/helloandroid/UranaiActivity.java
+++ app/src/main/java/xyz/kafeekantate/helloandroid/UranaiActivity.java
@@ -2,6 +2,7 @@ package xyz.kafeekantate.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UranaiActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class UranaiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uranai);
+
+        TextView text = (TextView) findViewById(R.id.uranaiText);
+        text.setText("中吉");
     }
 }
